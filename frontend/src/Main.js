d586b635df655c0c7808555f4da28a5aa7b5041e
tmachine <tmachine@sys08.PEC.IN>
2024-02-22 14:26:50 +0530

    frontend

---
+++ frontend/src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navigation from './Pages/Navigation'
+import Timeline from './Pages/Timeline/Timeline'
+
+
+function Main() {
+  return (
+    <div >
+      <Navigation/>
+      <Timeline/>
+
+      
+      </div>
+  )
+}
+
+export default Main
